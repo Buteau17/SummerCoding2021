0ea8d7b9e7d178132cdacee85152dd29bcd499f0
Buteau17 <80188697+Buteau17@users.noreply.github.com>
2021-07-11 18:26:53 +0800

    calendar

---
+++ calendar.py
@@ -0,0 +1,7 @@
+import calendar
+#imported the calendar library
+y=int(input("Input the year:"))
+#taking year as int input
+m=int(input("Input the month:"))
+#taking month as an int input
+print(calendar.month(y,m))
