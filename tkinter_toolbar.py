713115f9d26ef35753068a3c29916ba585d0ec14
Buteau17 <80188697+Buteau17@users.noreply.github.com>
2021-08-20 23:38:42 +0800

    Tkinter: Toolbar

---
+++ tkinter_toolbar.py
@@ -0,0 +1,32 @@
+from tkinter import *
+
+def function1():
+    print("Menu item clicked")
+
+root=Tk()
+mymenu=Menu(root)
+root.config(menu=mymenu)
+
+submenu=Menu(mymenu)
+
+mymenu.add_cascade(label="File", menu=submenu)
+
+
+submenu.add_command(label="Project", command=function1)
+submenu.add_command(label="Save", command=function1)
+
+submenu.add_separator()
+submenu.add_command(label="Exit", command=function1)
+
+newmenu= Menu(mymenu)
+mymenu.add_cascade(label="Edit", menu=newmenu)
+newmenu.add_command(label="Undo", command=function1)
+
+toolbar=Frame(root, bg="pink")
+insertbutton=Button(toolbar, text="Insert Files", command=function1)
+insertbutton.pack(side=LEFT, padx=2, pady=3)
+printbutton=Button (toolbar, text="print", command=function1)
+printbutton.pack(side=LEFT, padx=2, pady=3)
+
+toolbar.pack(side=TOP, fill=X)
+root.mainloop()
